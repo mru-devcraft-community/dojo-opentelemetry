f1ef4ac2e0bc6e56f0522c8d161ba6ef697a54d8
fadoul1 <fadouro@gmail.com>
2026-04-06 11:42:19 +0400

    conf: add scalar ui for net api

--- dotnet/src/ShopTrack.Api/Program.cs
+++ dotnet/src/ShopTrack.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using ShopTrack.Api.Data;
 using ShopTrack.Api.Endpoints;
 using ShopTrack.ServiceDefaults;
@@ -13,9 +14,17 @@ builder.Services.AddHttpClient("NotificationService", client =>
     client.BaseAddress = new Uri("https://httpbin.org/");
 });
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 // Ensure DB is created
 using (var scope = app.Services.CreateScope())
